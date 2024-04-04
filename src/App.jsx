f24bf7e934e35e62b7fa0f1739d78409a936a52a
amrmabdelazeem <amr.abdelazeem22@gmail.com>
2024-04-04 15:46:20 +0300

    Update index.html title and install react-router-dom
    Add BrowserRouter, Route and Routes components from react-router-dom in App.jsx
    
    Add react-router-dom as a dependency in package.json and update package-lock.json
    
    No changes made to node-sass and react dependencies.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./app.scss";
 
 function App() {
 
   return (
     <>
-    <header>Welcome</header>
+    
     </>
   )
 }
---
+++ src/pages/home/Home.js
@@ -0,0 +1,7 @@
+export default function home() {
+  return (
+    <section>
+      <h1>Home</h1>
+    </section>
+  );
+}
