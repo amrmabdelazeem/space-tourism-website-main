e562de45495f1772ddeb58b1d306616a9ffa2689
amrmabdelazeem <amr.abdelazeem22@gmail.com>
2024-04-07 17:40:20 +0300

    "Implement crew member selection with state and updated image sources"
    This commit updates the Crew.jsx file to use React's useState hook and display the appropriate crew member's information based on the currentIndex state. It also updates the image source for the crew member's photo to use the images.webp property from the data.json file. The crew.scss file has minor changes to match the updated HTML structure.

--- src/pages/crew/Crew.jsx
+++ src/pages/crew/Crew.jsx
@@ -1,7 +1,9 @@
 import "./crew.scss";
 import data from "../../data.json";
+import { useState } from "react";
 
 export default function Crew() {
+  const [currentIndex, setCurrentIndex] = useState(0);
   return (
     <main className="crew">
       <header>
@@ -9,22 +11,18 @@ export default function Crew() {
         <h5>Pick your crew</h5>
       </header>
       <article className="crew-photo">
-        <img src="assets/crew/image-douglas-hurley.png" alt="crew" />
+        <img src={data.crew[currentIndex].images.webp} alt="crew" />
       </article>
       <article className="crew-info">
         <div className="slider-dots">
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-
+          <span className="slider-dot"></span>
+          <span className="slider-dot"></span>
+          <span className="slider-dot"></span>
+          <span className="slider-dot"></span>
         </div>
-        <h3>Commander</h3>
-        <h4>Douglas Hurley</h4>
-        <p>
-          Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA
-          astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.
-        </p>
+        <h3> {data.crew[currentIndex].role}</h3>
+        <h4>{data.crew[currentIndex].name}</h4>
+        <p>{data.crew[currentIndex].bio}</p>
       </article>
     </main>
   );
