2fbd7cd04a5436daa964c492ebe2d5cab07d697c
amrmabdelazeem <amr.abdelazeem22@gmail.com>
2024-04-07 17:19:28 +0300

    Update Crew page with content and styles; adjust padding in Destination and variable SCSS files
    The Crew page was updated with content and styles, including a new component structure, images, text, and design elements. The Destination page's padding was slightly adjusted, and the $text-color-grey variable was added to the _variables.scss file.

--- src/pages/crew/Crew.jsx
+++ src/pages/crew/Crew.jsx
@@ -1,6 +1,31 @@
 import "./crew.scss";
 import data from "../../data.json";
 
-export default function Crew(){
-    return
+export default function Crew() {
+  return (
+    <main className="crew">
+      <header>
+        <span>02</span>
+        <h5>Pick your crew</h5>
+      </header>
+      <article className="crew-photo">
+        <img src="assets/crew/image-douglas-hurley.png" alt="crew" />
+      </article>
+      <article className="crew-info">
+        <div className="slider-dots">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+
+        </div>
+        <h3>Commander</h3>
+        <h4>Douglas Hurley</h4>
+        <p>
+          Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA
+          astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.
+        </p>
+      </article>
+    </main>
+  );
 }
