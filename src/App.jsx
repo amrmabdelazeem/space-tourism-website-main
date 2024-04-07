2e0d37360b19e0c672f11473873a914f6b255690
amrmabdelazeem <amr.abdelazeem22@gmail.com>
2024-04-07 16:09:24 +0300

    Add Crew page and route
    This diff adds a new page for "Crew" and the corresponding route in the navigation. The new page can be accessed via the updated route path "/crew". The Navbar component has been updated to include a link to the new Crew page.

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,19 @@ import Home from "./pages/home/Home";
 import Destination from "./pages/destination/destination";
 import "./app.scss";
 import Navbar from "./components/navbar/NavBar";
+import Crew from "./pages/crew/Crew";
 
 function App() {
   return (
     <>
       <Router>
-      <div className="app">
-      <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/destination" element={<Destination />}></Route>
-        </Routes>
+        <div className="app">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/destination" element={<Destination />}></Route>
+            <Route path="/crew" element={<Crew />}></Route>
+          </Routes>
         </div>
       </Router>
     </>
---
+++ src/pages/crew/Crew.jsx
@@ -0,0 +1,6 @@
+import "./crew.scss";
+import data from "../../data.json";
+
+export default function Crew(){
+    return
+}
