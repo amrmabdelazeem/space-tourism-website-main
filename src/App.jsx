a6ca154207abb44d69e6cc1254606509880352ad
amrmabdelazeem <amr.abdelazeem22@gmail.com>
2024-04-07 22:50:14 +0300

    Add Technology page and update app and page styles
    The changes include:
    
    * Adding a new page component for "Technology" in `src/pages/technology/Technology.jsx`.
    * Updating the `App.jsx` file to include a new route for the "Technology" page.
    * Updating the `app.scss` file to add a padding to the main content area.
    * Updating the `crew.scss` file to remove the padding from the crew page.
    * Updating the `destination.scss` file to remove the padding from the destination page.
    
    Note: The changes to the `app.scss` file add padding to the main content area, while the changes to the `crew.scss` and `destination.scss` files remove padding from the respective pages. This is done to maintain consistent spacing across the pages.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/home/Home";
 import Destination from "./pages/destination/destination";
 import "./app.scss";
 import Navbar from "./components/navbar/NavBar";
+import Technology from "./pages/technology/Technology";
 import Crew from "./pages/crew/Crew";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
             <Route path="/" element={<Home />}></Route>
             <Route path="/destination" element={<Destination />}></Route>
             <Route path="/crew" element={<Crew />}></Route>
+            <Route path="/tech" element={<Technology/>}></Route>
           </Routes>
         </div>
       </Router>
---
+++ src/pages/technology/Technology.jsx
@@ -0,0 +1,8 @@
+import "./technology.scss";
+import data from "../../data.json";
+
+export default function Technology() {
+  return <main className="tech">
+    <h1>Tech</h1>
+  </main>;
+}
