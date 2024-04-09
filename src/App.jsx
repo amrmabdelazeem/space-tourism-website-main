97b619d1c9a93023809e36e183890c1229668b3d
amrmabdelazeem <amr.abdelazeem22@gmail.com>
2024-04-09 14:39:37 +0300

    Update App.jsx: replace Navbar handleMenu function, add index path for Home route, and update all route paths to include starting slash
    Commit message:
    
    * Update App.jsx: handleMenu function refactor, add index path for Home route, and update all route paths to include starting slash
    
    Changes:
    
    * Replace the `handleMenu` function in the `App` component with an updated version that toggles the `toggle` state.
    * Add an `index` path to the `Home` route, so that it can be accessed with either `/` or `/home` URLs.
    * Update all route paths to include a starting slash (e.g., `/destination` instead of `destination`). This is a best practice to ensure that the routes work correctly regardless of the current URL.

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,24 @@ import Crew from "./pages/crew/Crew";
 import Sidebar from "./components/sidebar/Sidebar";
 import { useState } from "react";
 
-
 function App() {
   const [toggle, setToggle] = useState(false);
-  const handleMenu = () =>{
+  const handleMenu = () => {
     setToggle(!toggle);
-  }
+  };
   return (
     <>
       <Router>
         <div className="app">
-          <Navbar onOpen={handleMenu} show={toggle}/>
+          <Navbar onOpen={handleMenu} show={toggle} />
           <Sidebar onClose={handleMenu} hide={toggle} />
-            <Routes>
-              <Route path="/" element={<Home />}></Route>
-              <Route path="/destination" element={<Destination />}></Route>
-              <Route path="/crew" element={<Crew />}></Route>
-              <Route path="/technology" element={<Technology />}></Route>
-            </Routes>
+          <Routes>
+            <Route path={"/"} element={<Home />}></Route>
+            <Route index path={"/home"} element={<Home/>}></Route>
+            <Route path="destination" element={<Destination />}></Route>
+            <Route path="crew" element={<Crew />}></Route>
+            <Route path="technology" element={<Technology />}></Route>
+          </Routes>
         </div>
       </Router>
     </>
