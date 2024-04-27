93c77b51925c5a838b0b5479d086bbca11161fa5
amrmabdelazeem <amr.abdelazeem22@gmail.com>
2024-04-28 02:43:33 +0300

    "Refactored Destination page component structure and styles"

--- src/pages/destination/destination.jsx
+++ src/pages/destination/destination.jsx
@@ -13,44 +13,48 @@ export default function Destination() {
         <span>01</span>
         <h5>Pick your destination</h5>
       </header>
-      <img src={data.destinations[currentIndex].images.webp} alt="moon-image" />
-      <ul>
-        {names.map((dName, index) => {
-          return (
-            <li
-              key={dName}
-              style={
-                names[currentIndex] !== dName
-                  ? { borderBottom: "none" }
-                  : { borderBottom: "3px solid #ffffff" }
-              }
-            >
-              <a
-                href=""
-                onClick={(e) => {
-                  e.preventDefault();
-                  setCurrentIndex(index);
-                }}
-                style={names[currentIndex] !== dName ? { color: "#D0D6F9" } : { color: "#fff" }}
-              >
-                {dName}
-              </a>
-            </li>
-          );
-        })}
-      </ul>
-      <h2>{names[currentIndex]}</h2>
-      <p>{data.destinations[currentIndex].description}</p>
-      <hr />
-      <section className="articles">
-        <article>
-          <h3>AVG. DISTANCE</h3>
-          <h4>{data.destinations[currentIndex].distance}</h4>
+      <section className="main-content">
+        <img src={data.destinations[currentIndex].images.webp} alt="moon-image" />
+        <article className="content-container">
+          <ul>
+            {names.map((dName, index) => {
+              return (
+                <li
+                  key={dName}
+                  style={
+                    names[currentIndex] !== dName
+                      ? { borderBottom: "none" }
+                      : { borderBottom: "3px solid #ffffff" }
+                  }
+                >
+                  <a
+                    href=""
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setCurrentIndex(index);
+                    }}
+                    style={names[currentIndex] !== dName ? { color: "#D0D6F9" } : { color: "#fff" }}
+                  >
+                    {dName}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+          <h2>{names[currentIndex]}</h2>
+          <p>{data.destinations[currentIndex].description}</p>
         </article>
-        <article>
-          <h3>Est. travel time</h3>
-          <h4>{data.destinations[currentIndex].travel}</h4>
-        </article>
+        <hr />
+        <section className="articles">
+          <article className="article">
+            <h3>AVG. DISTANCE</h3>
+            <h4>{data.destinations[currentIndex].distance}</h4>
+          </article>
+          <article className="article">
+            <h3>Est. travel time</h3>
+            <h4>{data.destinations[currentIndex].travel}</h4>
+          </article>
+        </section>
       </section>
     </main>
   );
