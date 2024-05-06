8a73a9c37c1a2c517c4e3cdbfee1bfea6421670a
amrmabdelazeem <amr.abdelazeem22@gmail.com>
2024-05-06 14:43:41 +0300

    Add grid layout to Crew page and move crew photo to the right column

--- src/pages/crew/Crew.jsx
+++ src/pages/crew/Crew.jsx
@@ -6,32 +6,38 @@ export default function Crew() {
   const [currentIndex, setCurrentIndex] = useState(0);
   return (
     <main className="crew">
-    <div className="bg-moon-container"></div>
+      <div className="bg-moon-container"></div>
       <header>
         <span>02</span>
         <h5>Meet your crew</h5>
       </header>
-      <section className="crew-photo">
-        <img src={data.crew[currentIndex].images.webp} alt="crew" />
-      </section>
-      <section className="crew-info">
-        <div className="slider-dots">
-          {data.crew.map((member, index) => {
-            return (
-              <span
-                className="slider-dot"
-                key={member.name}
-                onClick={() => {
-                  setCurrentIndex(index);
-                }}
-                style={currentIndex === index ? {backgroundColor:'#ffffff'} : {backgroundColor:'#979797'}}
-              ></span>
-            );
-          })}
-        </div>
-        <h3> {data.crew[currentIndex].role}</h3>
-        <h4>{data.crew[currentIndex].name}</h4>
-        <p>{data.crew[currentIndex].bio}</p>
+      <section className="main-content">
+        <section className="crew-photo">
+          <img src={data.crew[currentIndex].images.webp} alt="crew" />
+        </section>
+        <section className="crew-info">
+          <div className="slider-dots">
+            {data.crew.map((member, index) => {
+              return (
+                <span
+                  className="slider-dot"
+                  key={member.name}
+                  onClick={() => {
+                    setCurrentIndex(index);
+                  }}
+                  style={
+                    currentIndex === index
+                      ? { backgroundColor: "#ffffff" }
+                      : { backgroundColor: "#979797" }
+                  }
+                ></span>
+              );
+            })}
+          </div>
+          <h3> {data.crew[currentIndex].role}</h3>
+          <h4>{data.crew[currentIndex].name}</h4>
+          <p>{data.crew[currentIndex].bio}</p>
+        </section>
       </section>
     </main>
   );
