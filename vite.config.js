0197bfeedf34d57c6b0368dfb08f2df0ac19986e
amrmabdelazeem <amr.abdelazeem22@gmail.com>
2024-05-13 23:19:33 +0300

    Update vite.config.js: uncommented `base` property with value "/space-tourism-website-main/"

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/space-tourism-website-main/"
+  // base: "/space-tourism-website-main/"
 })
