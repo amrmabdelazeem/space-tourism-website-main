7c29c04b70c90faede8fa8d3cfc4c16801eb1373
amrmabdelazeem <amr.abdelazeem22@gmail.com>
2024-05-13 12:35:23 +0300

    Remove `Link` import from react-router-dom in Sidebar component

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import "./sidebar.scss";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Sidebar({ onClose, hide }) {
   const routes = ["home", "destination", "crew", "technology"];
