b8e493232e88c77ca8a33657b6118060a69513a5
amrmabdelazeem <amr.abdelazeem22@gmail.com>
2024-05-13 12:48:41 +0300

    Change onClick handler for sidebar and SVG element

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -4,9 +4,9 @@ import { NavLink } from "react-router-dom";
 export default function Sidebar({ onClose, hide }) {
   const routes = ["home", "destination", "crew", "technology"];
   return (
-    <div className="sidebar" style={{ display: hide ? "flex" : "none" }} onClick={onClose}>
+    <div className="sidebar" style={{ display: hide ? "flex" : "none" }} >
       <button className="menu-btn" role="button" tabIndex="0">
-        <svg className="close-menu" width="20" height="21">
+        <svg className="close-menu" width="20" height="21" onClick={onClose}>
           <g fill="#D0D6F9" fillRule="evenodd">
             <path d="M2.575.954l16.97 16.97-2.12 2.122L.455 3.076z" />
             <path d="M.454 17.925L17.424.955l2.122 2.12-16.97 16.97z" />
@@ -16,7 +16,7 @@ export default function Sidebar({ onClose, hide }) {
       <div className="hr"></div>
       {routes.map((route, index) => {
         return (
-          <div className="route" key={route}>
+          <div className="route" key={route} onClick={onClose}>
             <NavLink to={route}>
               <span>0{index}</span>
               {route}
